eae0eef0ca64cd892c7b184e6b43e570806e2c30
Jose_Byteando <136005855+joseroyo3@users.noreply.github.com>
2026-01-02 19:37:10 +0100

    Add files via upload
    detalles del grupo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "121104566"
+    serviceWorkerVersion: "2928031951"
   }
 });
